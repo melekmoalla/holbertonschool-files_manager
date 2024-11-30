f3f244f627f295af260d3a4a84bb08f12e7674dc
melekmoalla <melekmoalla2468@gmail.com>
2024-11-30 21:01:23 +0100

    test and found problem

--- utils/db.mjs
+++ utils/db.mjs
@@ -24,27 +24,11 @@ class DBClient {
   }
 
   // Properly handle isAlive check
-  async isAlive() {
-    // If the database client is not initialized, attempt to connect
-    if (this.db === null) {
-      console.log('MongoDB not initialized, attempting to connect...');
-      const connectionSuccess = await this.connect();
-      if (!connectionSuccess) {
-        return false; // If the connection fails, return false
-      }
-    }
 
-    try {
-      // Ping MongoDB server to check if it's alive
-      await this.db.command({ ping: 1 });
-      console.log('MongoDB is alive');
-      return true; // If no error occurs, MongoDB is alive
-    } catch (error) {
-      // If pinging fails, return false
-      console.error('Error pinging MongoDB:', error);
-      return false;
-    }
+  isAlive() {
+    return !!this.db;
   }
+
 
   // Get the number of users in the users collection
   async nbUsers() {
